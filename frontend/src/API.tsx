9f7d1a07501eb2243954162cca19a92bb850288c
prushton2 <peterrushton418@gmail.com>
2025-09-25 16:20:14 -0400

    added env

--- frontend/src/API.tsx
+++ frontend/src/API.tsx
@@ -3,13 +3,13 @@ import axios from 'axios';
 import { Snapshot, Timeframe } from './models/GenericModels';
 
 export async function getLatestTrainData(): Promise<Snapshot> {
-    let response = await axios.get('http://localhost:3000/v1/live')
+    let response = await axios.get(`${import.meta.env.VITE_APP_BACKEND_URL}/v1/live`)
     let obj = response.data;
     return obj as Snapshot;
 }
 
 export async function getHistoricalTrainData(time: number): Promise<Timeframe> {
-    let response = await axios.get(`http://localhost:3000/v1/historical?t=${time}`)
+    let response = await axios.get(`${import.meta.env.VITE_APP_BACKEND_URL}/v1/historical?t=${time}`)
     let obj = response.data;
 
     let timeframe: Timeframe = {snapshots: new Map<number, Snapshot>() } as Timeframe;
